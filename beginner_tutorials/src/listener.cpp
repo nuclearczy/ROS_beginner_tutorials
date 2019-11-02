14263c7d04d8d33d7b412ff9aa150af216be6341
ZuyangCao <nuclearczy@hotmail.com>
2019-11-02 17:02:04 -0400

    [ROS Service] Added a service in Listener Toggles the listener's status

--- beginner_tutorials/src/listener.cpp
+++ beginner_tutorials/src/listener.cpp
@@ -1,56 +1,51 @@
-/*Copyright (C) 2019 Zuyang Cao */
+/** Copyright (c) 2019   Zuyang Cao
+ *  @file       listener.cpp
+ *  @brief      A simple ROS beginner tutorial for subscriber and service.
+ *  @license    This project is released under the BSD-3-Clause License.
+ */
+#include <std_srvs/Empty.h>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
-/**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
+/** @brief Indicates the status of this node. */
+bool listenStatus = true;
+
+/** @brief   Callback function for service which switch the listener's status
+ *  @param   Service request type variable
+ *  @param   Service response type variable
+ *  @return  boolean true 
  */
-void chatterCallback(const std_msgs::String::ConstPtr& msg) {
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+bool toggleListenStatus (std_srvs::Empty::Request &req, std_srvs::Empty::Response &resp){
+  listenStatus = !listenStatus ;
+  ROS_WARN_STREAM("Now entering " << (listenStatus ? "Listening" : "Deaf") << " status." << std::endl);
+  return true;
 }
 
+
+/** @brief   Callback function for subscriber
+ *  @param   Msg type message
+ */
+void chatterCallback(const std_msgs::String::ConstPtr& msg) {
+  if (listenStatus){
+    ROS_INFO_STREAM("I heard: " << msg->data);
+  }
+}
+
+/** @brief   Main function for the subscriber
+ *  @param   ROS argc
+ *  @param   ROS argv
+ *  @return  0 
+ */
 int main(int argc, char **argv) {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
+  
   ros::init(argc, argv, "listener");
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
-  ros::NodeHandle n;
+  ros::NodeHandle nh;
 
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
-  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+  ros::ServiceServer server = nh.advertiseService("toggle_listen_status", &toggleListenStatus);
 
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
+  ros::Subscriber sub = nh.subscribe("chatter", 1000, chatterCallback);
+
   ros::spin();
 
   return 0;
